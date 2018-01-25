7d3c1f17ec1139cd35cb0d67752237b159c0947f
Alessio Russo <alessiomrusso@gmail.com>
2018-01-25 22:38:29 +0100

    Create passport strategy for jwt authentication

--- router.js
+++ router.js
@@ -1,5 +1,14 @@
 const Auth = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', {
+    session: false
+})
 
 module.exports = app => {
+    app.get('/', requireAuth, function(req, res){
+        res.send('<div>Hi there!</div>')
+    })
     app.post('/signup', Auth.signup);
 };
